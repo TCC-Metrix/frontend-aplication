440c888a6e5d999868bd04dceddfc7530cae8923
matheusaprigio11 <aprigiomatheus24@gmail.com>
2024-03-04 16:34:09 -0300

    :sparkless: Add - Tabela Instrumento

---
+++ src/components/TableMovSaidaUso/TableMovSaidaUso.tsx
@@ -0,0 +1,20 @@
+import './TableMovSaidaUso.css'
+
+const TableMovSaidaUso = () => {
+  return (
+    <div>
+      <table className='table-saidauso'>
+        <tr className='firt-line'>
+          <th>Código</th>
+          <th>Descrição</th>
+          <th>Referência Adicional</th>
+        </tr>
+        <tr>
+          <td>Nenhum instrumento adicionado</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
+
+export default TableMovSaidaUso
--- src/pages/Movimentacoes/SaidaParaUso/MovSaidaUso.tsx
+++ src/pages/Movimentacoes/SaidaParaUso/MovSaidaUso.tsx
@@ -1,16 +1,21 @@
 import NavBar from "../../../components/Navbar/Navbar";
 import "./MovSaidaUso.css";
 import ButtonDark from "../../../components/ButtonDark/ButtonDark";
-
+import TableMovSaidaUso from "../../../components/TableMovSaidaUso/TableMovSaidaUso";
 
 export const MovSaidaUso = () => {
 	return (
 		<section>
 			<NavBar />
 			<div className="container-main">
-				<h1>Saída para uso</h1>
-				<p>Instrumento</p>
-				<ButtonDark name="+ Adicionar" />
+				<div className="top-information">
+					<h1>Saída para uso</h1>
+					<p>Instrumento</p>
+					<ButtonDark name="+ Adicionar" />
+				</div>
+				<div>
+					<TableMovSaidaUso />
+				</div>
 			</div>
 		</section>
 	);
