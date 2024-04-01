dc9c2cffa06e27ea929cc743a428821daf43cf87
juliagbmaciel <juliagabrielle863@gmail.com>
2024-04-01 13:21:35 -0300

    updt

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import {Routes, Route} from "react-router-dom"
-import { MoveUseOutput } from "./pages/Movements/SaidaParaUso/MovUseOutput";
+import { MoveUseOutput } from "./pages/Movements/UseOutput/MovUseOutput";
 import './App.css'
 
 function App() {
