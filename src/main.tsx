77bc4e821a221b4475cf62bdd3a9c34db9c431cf
matheusaprigio11 <aprigiomatheus24@gmail.com>
2024-04-04 11:26:40 -0300

    :sparkles: add SSO Login

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,25 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { MsalProvider } from "@azure/msal-react";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { msalConfig } from "./authSSO/authConfig.tsx";
 
 const queryClient = new QueryClient({
 	defaultOptions: { queries: { retry: 3, retryDelay: 1000 } },
 });
 
+const msalInstance = new PublicClientApplication(msalConfig);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<QueryClientProvider client={queryClient}>
-				<App />
-				<ReactQueryDevtools initialIsOpen={false} />
-			</QueryClientProvider>
-		</BrowserRouter>
+		<MsalProvider instance={msalInstance}>
+			<BrowserRouter>
+				<QueryClientProvider client={queryClient}>
+					<App />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</QueryClientProvider>
+			</BrowserRouter>
+		</MsalProvider>
 	</React.StrictMode>
 );
