92100100747d8a155c535e8f8d4cc60e6930ca8f
matheusaprigio11 <aprigiomatheus24@gmail.com>
2024-04-08 08:52:52 -0300

    :sparkles: add register screens

---
+++ src/pages/Registers/InstrumentRegister.tsx
@@ -0,0 +1,13 @@
+import './InstrumentRegister.css'
+
+const InstrumentRegister = () => {
+  return (
+    <div>
+      <div>
+        <h1>CADASTRARRRRRRRRRRRR INSTRUMENTOS</h1>
+      </div>
+    </div>
+  )
+}
+
+export default InstrumentRegister
