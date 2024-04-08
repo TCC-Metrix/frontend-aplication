1581807ba40c8f202a8438d88bfb3f00c8656b0e
matheusaprigio11 <aprigiomatheus24@gmail.com>
2024-04-08 16:51:06 -0300

    :construction: Add links on router

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -7,7 +7,6 @@ export {
   export { default as Table } from "./Table/Table";
   export { default as Pagination } from "./Pagination/Pagination";
   export { default as Popup } from "./Popup/Popup";
-  export { default as LoadingPage } from "./LoadingPage/LoadingPage";
   export { default as Menu } from "./Menu/Menu";
   export { default as Checkbox } from "./Checkbox/Checkbox";
   export { default as Button } from "./Buttons/Button";
