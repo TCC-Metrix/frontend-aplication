06f5c43fe92be3a6bf0fae162cd84a6097d74a91
matheusaprigio11 <aprigiomatheus24@gmail.com>
2024-04-10 10:28:03 -0300

    :sparkles: add metrix logo and fix navbar

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import Logo from "./assets/images/rexroth-logo.gif";
 import Profile from "./assets/images/profileImg.jpg";
 import Loading from './assets/images/loading_spinner.gif'
+import LogoMetrix from './assets/images/metrix-logo-dark.png'
 
 
-export { Logo, Profile, Loading };
+export { Logo, Profile, Loading, LogoMetrix };
