0c853cb3e007193c255d14da13753dd6388700a4
ViniTex231 <viniteixeira2828@gmail.com>
2024-04-17 14:41:58 -0300

    feat: 50%

--- src/pages/Registers/Laboratory/LaboratoryRegister.tsx
+++ src/pages/Registers/Laboratory/LaboratoryRegister.tsx
@@ -8,14 +8,89 @@ import { z } from "zod";
 import { RotatingLines } from "react-loader-spinner";
 import { LaboratoryRegisterPost } from "../../../utils/interfaces/Interfaces";
 import { usePostLaboratoryRegister } from "../../../services/useMutation";
+import { useState } from "react";
+
+const schema = z.object({
+	name: z
+		.string()
+		.min(1, "Campo obrigatorio")
+		.refine((value) => !/^\s+$/.test(value), {
+			message: "Nome não pode conter apenas espaços em branco",
+		}),
+})
+
+type FormFields = z.infer<typeof schema>
 
 const LaboratoryRegister = () => {
 	const setActiveNavbar = useNavbarStore((state) => state.setActiveNavbar);
+	const setPopupType = usePopupStore((state) => state.setPopupType);
+	const setPopupBody = usePopupStore((state) => state.setPopupBody);
+	const setPopupTitle = usePopupStore((state) => state.setPopupTitle);
+	const setIsPopupActive = usePopupStore((state) => state.setIsPopupActive);
+	const setPopupFunction = usePopupStore((state) => state.setPopupFunction);
+	const [isLoadingPostLaboratoryRegister, setIsLoadingPostLaboratoryRegister] =
+		useState<boolean>(false);
 	const {
 		register,
 		formState: { errors },
 		handleSubmit,
-	} = useForm();
+	} = useForm<FormFields>({ resolver: zodResolver(schema) });
+
+	const createPopup = (
+		type: string,
+		title: string,
+		body: string,
+		btnFunction: () => void
+	) => {
+		setPopupType(type);
+		setPopupTitle(title);
+		setPopupBody(body);
+		setPopupFunction(() => {
+			setPopupBody("");
+			setPopupTitle("");
+			setPopupType("none");
+			btnFunction();
+		});
+		setIsPopupActive(true);
+	};
+
+	const postLaboratoryMutation = usePostLaboratoryRegister();
+
+	const handlePostLaboratoryRegister: SubmitHandler<LaboratoryRegisterPost> = (
+		data
+	) => {
+		setIsLoadingPostLaboratoryRegister(true);
+		postLaboratoryMutation.mutate(data, {
+			onSettled: (data, error) => {
+				if (error) {
+					setTimeout(() => {
+						setIsLoadingPostLaboratoryRegister(false);
+						console.error("Ocorreu um erro:", error);
+						createPopup(
+							"error",
+							"Erro interno do servidor",
+							"Estamos com problemas em nosso servidor, tente novamente",
+							() => {
+								setIsPopupActive(false);
+							}
+						);
+					}, 1000);
+					return;
+				} else {
+					console.log(data);
+					setIsLoadingPostLaboratoryRegister(false);
+					createPopup(
+						"feedback",
+						"Movimentação realizada com sucesso",
+						"",
+						() => {
+							setIsPopupActive(false);
+						}
+					);
+				}
+			},
+		});
+	};	
 
 	const onSubmit = (data: FieldValues) => {
 		console.log(data);
@@ -57,7 +132,18 @@ const LaboratoryRegister = () => {
 								onClickFunction={handleSubmit(onSubmit)}
 								className="btn btn-secondary"
 							>
-								Confirmar
+								{isLoadingPostLaboratoryRegister ? (
+									<RotatingLines
+										visible={true}
+										strokeWidth="5"
+										animationDuration="0.75"
+										ariaLabel="rotating-lines-loading"
+										strokeColor="#fff"
+										width="20"
+									/>
+								) : (
+									<>Confirmar</>
+								)}
 							</Button>
 						</div>
 					</form>
