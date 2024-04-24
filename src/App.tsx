287dda110caa3cf447dea1b6080f77bbade05a88
matheusaprigio11 <aprigiomatheus24@gmail.com>
2024-04-24 16:53:42 -0300

    :bug: fix all bugs of merge

--- src/App.tsx
+++ src/App.tsx
@@ -15,80 +15,72 @@ import AreaRegister from "./pages/Registers/Area/AreaRegister";
 
 import UseReturn from "./pages/Movements/UseReturn/UseReturn";
 
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
-
 function App() {
-  const activeNavbar = useNavbarStore((state) => state.activeNavbar);
-  const setActiveNavbar = useNavbarStore((state) => state.setActiveNavbar);
-  const popupBody = usePopupStore((state) => state.popupBody);
-  const isPopupActive = usePopupStore((state) => state.isPopupActive);
-  const popupTitle = usePopupStore((state) => state.popupTitle);
-  const popupType = usePopupStore((state) => state.popupType);
-  const popupBtnFunction = usePopupStore((state) => state.popupBtnFunction);
+	const activeNavbar = useNavbarStore((state) => state.activeNavbar);
+	const setActiveNavbar = useNavbarStore((state) => state.setActiveNavbar);
+	const popupBody = usePopupStore((state) => state.popupBody);
+	const isPopupActive = usePopupStore((state) => state.isPopupActive);
+	const popupTitle = usePopupStore((state) => state.popupTitle);
+	const popupType = usePopupStore((state) => state.popupType);
+	const popupBtnFunction = usePopupStore((state) => state.popupBtnFunction);
 
+	const location = useLocation();
 
-  const location = useLocation();
+	useEffect(() => {
+		setActiveNavbar(false);
+	}, [location]);
 
-  useEffect(() => {
-	  setActiveNavbar(false)
-  }, [location]);
+	return (
+		<>
+			<Popup
+				isActive={isPopupActive}
+				title={popupTitle}
+				type={popupType}
+				body={popupBody}
+				btnFunction={popupBtnFunction}
+			/>
 
-
-  return (
-      <>
-        <Popup
-          isActive={isPopupActive}
-          title={popupTitle}
-          type={popupType}
-          body={popupBody}
-          btnFunction={popupBtnFunction}
-        />
-
-        <ProtectedRoute>
-          <NavBar
-            activeNavbar={activeNavbar}
-            setActiveNavbar={setActiveNavbar}
-          />
-        </ProtectedRoute>
-
-        <Routes>
-          <Route path="/login" element={<PreLogin />}></Route>
-          <Route
-            path="/movement/use/output"
-            element={
-              <ProtectedRoute>
-                <MoveUseOutput />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route
-            path="/register/instrument"
-            element={
-              <ProtectedRoute>
-                <InstrumentRegister />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route
-            path="/error"
-            element={
-              <ProtectedRoute>
-                <ErrorPage />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route
-            path="/register/laboratory"
-            element={
-              <ProtectedRoute>
-                <LaboratoryRegister />
-              </ProtectedRoute>
-            }
-          ></Route>
+			<ProtectedRoute>
+				<NavBar activeNavbar={activeNavbar} setActiveNavbar={setActiveNavbar} />
+			</ProtectedRoute>
 
 			<Routes>
+				<Route path="/login" element={<PreLogin />}></Route>
+				<Route
+					path="/movement/use/output"
+					element={
+						<ProtectedRoute>
+							<MoveUseOutput />
+						</ProtectedRoute>
+					}
+				></Route>
+				<Route
+					path="/register/instrument"
+					element={
+						<ProtectedRoute>
+							<InstrumentRegister />
+						</ProtectedRoute>
+					}
+				></Route>
+				<Route
+					path="/error"
+					element={
+						<ProtectedRoute>
+							<ErrorPage />
+						</ProtectedRoute>
+					}
+				></Route>
+				<Route
+					path="/register/laboratory"
+					element={
+						<ProtectedRoute>
+							<LaboratoryRegister />
+						</ProtectedRoute>
+					}
+				></Route>
 				<Route path="/login" element={<PreLogin />}></Route>
 				<Route
 					path="/movement/use/output"
@@ -106,35 +98,33 @@ function App() {
 						</ProtectedRoute>
 					}
 				></Route>
-		
-          <Route
-            path="/register/family"
-            element={
-              <ProtectedRoute>
-                <FamilyRegister />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route
-            path="/register/supplier"
-            element={
-              <ProtectedRoute>
-                <SupplierRegister />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route
-            path="/register/area"
-            element={
-              <ProtectedRoute>
-                <AreaRegister />
-              </ProtectedRoute>
-            }
-          ></Route>
-        </Routes>
-      </>
-
-  );
+				<Route
+					path="/register/family"
+					element={
+						<ProtectedRoute>
+							<FamilyRegister />
+						</ProtectedRoute>
+					}
+				></Route>
+				<Route
+					path="/register/supplier"
+					element={
+						<ProtectedRoute>
+							<SupplierRegister />
+						</ProtectedRoute>
+					}
+				></Route>
+				<Route
+					path="/register/area"
+					element={
+						<ProtectedRoute>
+							<AreaRegister />
+						</ProtectedRoute>
+					}
+				></Route>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
